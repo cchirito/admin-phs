95526892faa4d6b1edae5ed3427a6bad5e11653b
Carlos Alexander <cchirito@phsperu.com>
2017-03-06 09:51:10 -0500

    Subida de datos completo

---
+++ application/controllers/backend.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Backend extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	$this->load->library('encrypt');
+	}
+
+	public function index() {
+		
+		$this->load->view('backend/login_view');
+	}
+
+	public function dashboard() {
+		$this->load->view('backend/inc/header');
+		$this->load->view('backend/dashboard_view');
+		$this->load->view('backend/inc/footer');
+	}
+
+	public function ajustes() {
+		$this->load->view('backend/inc/header');
+		$this->load->view('backend/settings_view');
+		$this->load->view('backend/inc/footer');
+	}
+
+	public function paginas($page = "") {
+		if($page != "") {
+			$this->load->view('backend/inc/header');
+			$this->load->view('backend/page_view');
+			$this->load->view('backend/inc/footer');
+		} else {
+			echo "error";
+		}
+		
+	}
+	
+}
